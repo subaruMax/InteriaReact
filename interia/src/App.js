2d3e38d78075c667774414dd7230c998e6918669
EllisiumMax <sailorjerry777@gmail.com>
2021-04-19 21:35:47 +0300

    Modified app for deployment

--- interia/src/App.js
+++ interia/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import React from "react";
 import NavBar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
@@ -7,13 +7,13 @@ import Services from "./components/Services/Services";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/interia_react/">
         <NavBar />
         <main>
-        <Route exact path="/"></Route>
-        <Route path="/about"></Route>
-        <Route path="/services" render={() => <Services />}></Route>
-        <Route path="/projects"></Route>
+          <Route exact path="/"></Route>
+          <Route path="/about"></Route>
+          <Route path="/services" render={() => <Services />}></Route>
+          <Route path="/projects"></Route>
         </main>
         <Footer />
       </BrowserRouter>
